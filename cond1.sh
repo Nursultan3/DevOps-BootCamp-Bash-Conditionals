fd1bfd83cb5f05973a5e9838d576c9bc338a9516
Nursultan Dauletov <dauletoviitu@gmail.com>
2023-01-16 22:58:06 +0600

    finished

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-IFS=',' read -r -a array <<< "$1"
+#IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+sum=0; 
+for i in ${@}; 
+do
+  if (($i%2==0));
+  then
+  sum=$i+$sum
+  fi 
+done;
+echo $sum |bc
--- temp.sh
+++ temp.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
 # Place your code here
+string=$1
+Cel="C"
+cel="c"
+Kel="K"
+kel="k"
+a=
+b=
+c=
+k= 
+
+case $string in
+   *"$Cel"*)
+   a=`echo "${string%C} + 273" | bc`
+   echo "${a}K"
+   ;;
+   *"$Kel"*)
+   b=`echo "${string%K} - 273" |bc`
+   echo "${b}C"
+   ;;
+   *"$cel"*)
+   c=`echo "${string%c} + 273" |bc`
+   echo "${c}K"
+   ;;
+   *"$kel"*)
+   k=`echo "${string%k} + 273" |bc`
+   echo "${k}K"
+   ;;
+esac 
