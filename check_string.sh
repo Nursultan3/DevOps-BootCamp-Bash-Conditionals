a79ebd56467a4c9fabc9585c52ea15b7f245c725
Nursultan Dauletov <dauletoviitu@gmail.com>
2023-01-19 01:46:35 +0600

    finally

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 # Place your code here
+string="${*}"
+
+echo "Numbers: $num", "Letters: $letters", "Symbols: $sym"
+#for i in "$string
+#do
+#done
+a=$(echo "$string" | tr -dc '0-9' | wc -c)
+num=$(echo "${a// /+}" | bc)
+b=$(echo "$string" | tr -dc 'A-z' | wc -c)
+letters=$(echo "${b// /+}" | bc)
+c=$(echo "$string" | tr -dc '[:punct:]' | wc -c)
+sym=$(echo "${c// /+}" | bc)
